9e431dc6e69a8fa6f0df1178829b955817bb639f
unknown <astighfar2000@gmail.com>
2024-01-09 20:46:41 +0100

    first

---
+++ backend/training-center/src/main/java/com/fst/trainingcenter/TrainingCenterApplication.java
@@ -0,0 +1,13 @@
+package com.fst.trainingcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingCenterApplication.class, args);
+	}
+
+}
---
+++ backend/training-center/src/test/java/com/fst/trainingcenter/TrainingCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fst.trainingcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
